693406f22676e02af64d0b8c77ea160a4ed2ff65
yorumusb <yorumusb@gmail.com>
2023-05-08 13:55:31 +0300

    start

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("selam");
+    const blog = {
+        id: 1,
+        title: "Blog title",
+        description: "Blog description",
+    };
+    res.send(blog);
 });
 
 const port = 3000;
