da8762b60d7009d6709f4c4d105ddbbefcf8cecf
yorumusb <yorumusb@gmail.com>
2023-05-08 20:06:18 +0300

    Add Template Engine

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
+// Routes
 app.get("/", (req, res) => {
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description",
-    };
-    res.send(blog);
+    res.render("index");
+});
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+app.get("/add_post", (req, res) => {
+    res.render("add_post");
+});
+app.get("/post", (req, res) => {
+    res.render("post");
 });
 
 const port = 3000;
